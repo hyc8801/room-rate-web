8bfebd7fb0a035a853e5a1425e1fa9faeec626ac
hyc <yongchuan.huang@socialcredits.cn>
2023-02-09 11:34:46 +0800

    ✨ feat: 新增区域折线图

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,16 +6,22 @@ import "./index.less";
 import { AREA_LIST, SECOND_HOUSE_TYPE } from './constant';
 import { Select } from 'antd';
 
+const areaFieldNames = {
+  label: "name",
+  value: "name"
+}
+
 
 const HomePage = () => {
 
   const [type, setType] = useState('quoted_price')
-  const { data } = useRequest(getSecondHouse)
+  const [area, setArea] = useState('重庆')
+  const { data = {} } = useRequest(getSecondHouse)
 
   const option = useMemo(() => {
     return {
       title: {
-        text: SECOND_HOUSE_TYPE.find((item) => item.value === type)?.label
+        text: AREA_LIST.find((item) => item.name === type)?.label
       },
       legend: {},
       grid: {
@@ -32,6 +38,9 @@ const HomePage = () => {
       },
       xAxis: {
         type: 'time',
+        axisLabel: {
+          formatter: "{yyyy}-{MM}-{dd}"
+        },
       },
       yAxis: {
         type: 'value',
@@ -43,10 +52,62 @@ const HomePage = () => {
     }
   }, [data, type])
 
+  const areaOption = useMemo(() => {
+    const areaData: any = {}
+    AREA_LIST.map((areatItem, index) => {
+      areaData[areatItem.name] = []
+      // { '重庆': [{type: 'line', name: 'quoted_price', data: []}]}
+      SECOND_HOUSE_TYPE.map((typeItem) => {
+        areaData[areatItem.name].push({
+          type: "line",
+          name: typeItem.label,
+          data: data[typeItem.value]?.[index]?.data
+        })
+      })
+    })
+    console.log(areaData)
+    console.log(areaData?.[area])
+    return {
+      title: {
+        text: area
+      },
+      legend: {},
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          animation: false
+        }
+      },
+      xAxis: {
+        type: 'time',
+        axisLabel: {
+          formatter: "{yyyy}-{MM}-{dd}"
+        },
+      },
+      yAxis: {
+        type: 'value',
+        min: (value: any) => {
+          return value.min - 20
+        },
+      },
+      series: areaData?.[area] || []
+    }
+  }, [data, area])
+  console.log(option)
+  console.log(areaOption)
   return (
     <div className='home-page'>
       <Select options={SECOND_HOUSE_TYPE} value={type} onChange={setType} />
       <ReactECharts option={option} />
+      <br />
+      <Select options={AREA_LIST} value={area} onChange={setArea} fieldNames={areaFieldNames} />
+      <ReactECharts option={areaOption} />
     </div>
   )
 }
